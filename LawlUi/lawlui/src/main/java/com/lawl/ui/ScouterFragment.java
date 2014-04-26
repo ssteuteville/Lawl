a54fecfee83146dc0ceaeb8f39962383b4044894
Shane Steuteville <ssteuteville@gmail.com>
2014-04-26 16:41:17 -0700

    Started working on ScoutFragment -- Example code
    I started making progress on the Scouting Fragment. As of now it makes an API call that takes a string of comma separated names. I parse the id's from the response and make an API call for each id. I then update the UI onSuccess for each API call. The whole time I keep a count variable to keep track of when to disable the progress bar. I added lots of comments since this is our first time using all of the components together.

--- LawlUi/lawlui/src/main/java/com/lawl/ui/ScouterFragment.java
+++ LawlUi/lawlui/src/main/java/com/lawl/ui/ScouterFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,16 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class ScouterFragment extends Fragment {
 
@@ -21,6 +31,7 @@ public class ScouterFragment extends Fragment {
     TextView textView;
     String summoner_names;
     ProgressBar progressBar;
+    RiotApiClient client;
 
     public ScouterFragment() {
         // Required empty public constructor
@@ -40,6 +51,8 @@ public class ScouterFragment extends Fragment {
         editText = (EditText) v.findViewById(R.id.scouter_edit_text);
         editText.setHint("Please enter summoner name(s)");
 
+        client = new RiotApiClient("0b63c21d-b03a-4c25-b481-57d853f29a08");
+
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
@@ -55,15 +68,57 @@ public class ScouterFragment extends Fragment {
         return v;
     }
 
-    private void handleText(String string) {
-
-        textView.setText(string);
+    private void handleText(String names) {
+        final List<String> name_list = Arrays.asList(names.split("\\s*,\\s*"));
+        String url = String.format("/api/lol/%s/v1.4/summoner/by-name/%s?", "na", names);
         progressBar.setVisibility(View.VISIBLE);
-        editText.setVisibility(View.INVISIBLE);
+
+        client.get(url, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(final JSONObject response)
+            {
+                try {
+                    final HashMap<String, String> ids = new HashMap<String,String>();//just testing out how hashmaps work in java and providing an example of persisting an object into the handler below. aka making it final
+                    final IntWrapper count = new IntWrapper(0);//since variables must be declared constant in order to be persisted into a handler we have to use an "IntWrapper"
+                    for(final String name : name_list) //java foreach loop -> do stuff for every name in the list
+                    {
+                        String id = response.getJSONObject(name).get("id").toString(); //get the id associated with a name
+                        ids.put(name, id); //add the id - name pair to hashmap
+                        final String id_url = String.format("/api/lol/na/v1.3/stats/by-summoner/%s/summary?season=SEASON4&", id); //format url properly. note: all urls will either have ? or & at the end. API key is inserted by client.
+
+                        Log.e("XXXXXXXXXXXXXXXXXX", id_url); //debug statement
+
+                        client.get(id_url, null, new JsonHttpResponseHandler(){ //make a new request for each name in the list
+                            @Override
+                            public void onSuccess(JSONObject id_response)
+                            {
+                                    textView.append( name + "'s id is: " + ids.get(name) + " Here is their data: \n\n" +  id_response.toString() + "\n\n");//make ui changes
+                                    count.integer = count.integer + 1;
+                                    if(count.integer == name_list.size())
+                                        progressBar.setVisibility(View.GONE);
+                            }
+                        });
+                    }
+                }
+                catch(JSONException ex)
+                {
+                    textView.setText(name_list.toString());
+                }
+
+            }
+        });
 
 
 
     }
 
 
+}
+
+class IntWrapper {
+    public IntWrapper(int i)
+    {
+        this.integer = i;
+    }
+    public int integer;
 }
